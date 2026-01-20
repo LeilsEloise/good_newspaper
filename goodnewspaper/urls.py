a752cc923ece1941693acfb9b370f8e1429b6524
LeilsEloise <leilahhodgson@gmail.com>
2026-01-20 11:14:57 +0000

    Updated project urls.py to use include method from new app urls.py

--- goodnewspaper/urls.py
+++ goodnewspaper/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('newstories/', news_story, name='newstories'),
+    path('newstories/', include('newstories.urls')),
 ]
