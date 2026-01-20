adc3e25553a19043700ae8968b566510e74d45c2
LeilsEloise <leilahhodgson@gmail.com>
2026-01-20 12:59:39 +0000

    Added Secret Key variable to settings to protect database application

--- goodnewspaper/settings.py
+++ goodnewspaper/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mv4(itnf5y$k--q7r&puk#d%^w59@y&feyz8$3hv*o4+$d$_e='
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
