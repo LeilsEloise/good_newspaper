66658a473b6942990f3d5a1e5c6b261194e559e3
LeilsEloise <leilahhodgson@gmail.com>
2026-01-26 21:58:55 +0000

    Fixed login, register and logout redirect

--- goodnewspaper/urls.py
+++ goodnewspaper/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from newstories.views import ArticleList
 
 urlpatterns = [
+    path('', ArticleList.as_view(), name='home'),
     path('about/', include('about.urls')),
-    path("accounts/", include("allauth.urls")),
+    path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('newstories/', include('newstories.urls')),
