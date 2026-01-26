01c185f9a8902c471358c77bda6830b4d7843b3e
LeilsEloise <leilahhodgson@gmail.com>
2026-01-26 12:28:10 +0000

    added article_detail view

--- newstories/views.py
+++ newstories/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Article
 
@@ -7,3 +7,26 @@ class ArticleList(generic.ListView):
     queryset = Article.objects.filter(status=1).order_by("-created_on")
     template_name = "newstories/index.html"
     paginate_by = 6
+
+def article_detail(request, slug):
+    """
+    Display an individual :model:`newstories.Article`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`newstories.Article`.
+
+    **Template:**
+
+    :template:`newstories/article_detail.html`
+    """
+
+    queryset = Article.objects.filter(status=1)
+    article = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "newstories/article_detail.html",
+        {"article": article },
+    )
