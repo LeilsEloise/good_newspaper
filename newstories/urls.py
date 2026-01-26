1f2e0b5ff88674f1d1845e62ef12bc648e346d84
LeilsEloise <leilahhodgson@gmail.com>
2026-01-26 13:21:43 +0000

    found issue was that article_detail was not named as html file, updated this and can view articles now

--- newstories/urls.py
+++ newstories/urls.py
@@ -1,6 +1,8 @@
 from . import views
 from django.urls import path
 
+app_name = 'newstories'
+
 urlpatterns = [
     path('', views.ArticleList.as_view(), name='home'),
     path('<slug:slug>/', views.article_detail, name='article_detail'),
