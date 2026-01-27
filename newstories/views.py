1565b1d8eed355e6be0c9bbc83692e334b770ab8
LeilsEloise <leilahhodgson@gmail.com>
2026-01-27 12:43:10 +0000

    Users now receive a success message when posting comments

--- newstories/views.py
+++ newstories/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.contrib import messages
 from .models import Article
+from .forms import CommentForm
 
 # Create your views here.
 class ArticleList(generic.ListView):
@@ -24,15 +26,34 @@ def article_detail(request, slug):
 
     queryset = Article.objects.filter(status=1)
     article = get_object_or_404(queryset, slug=slug)
-    comments = article.comments.all().order_by("-created_on")
-    comment_count = article.comments.filter(approved=True).count()
+
+    comments = article.comments.filter(approved=True).order_by("-created_on")
+    comment_count = comments.count()
+
+    comment_form = CommentForm()
+
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.article = article
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and awaiting approval'
+            )
+
+            # Reset form after successful submit
+            comment_form = CommentForm()
 
     return render(
         request,
         "newstories/article_detail.html",
-    {
-        "article": article,
-        "comments": comments,
-        "comment_count": comment_count,
-    },
+        {
+            "article": article,
+            "comments": comments,
+            "comment_count": comment_count,
+            "comment_form": comment_form,
+        },
     )
